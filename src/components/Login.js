22e203a611dc0e04f74366582c0e441b6ab29986
jasonsaini <jasonsaini.exe@gmail.com>
2022-10-16 19:03:40 -0400

    Added login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,5 +7,19 @@ function Login()
         event.preventDeafult();
 
         alert("doIt()");
-    }
-}
+    };
+
+    return (
+        <div id = "loginDiv">
+            <form onSubmit = {doLogin}>
+            <span id = "inner-title"> PLEASE LOG IN </span><br />
+            <input type = "text" id = "loginName" placeholder='Username'/><br />
+            <input type = "password" id = "loginPassword" placeholder='Password' /><br />
+            <input type = "submit" id = "loginButton" class ="buttons" value = "Do it" onClick={doLogin}/>
+            </form>
+            <span id = "loginResult"></span>
+        </div>
+    );
+};
+
+export default Login;
